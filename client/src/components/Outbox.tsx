74b25df2dc99071dfd72646b43f559df1c841c93
Danil Melkozerov <danmelk.dev@gmail.com>
2022-12-12 22:02:54 +0400

    chore: initial client folder

---
+++ client/src/components/Outbox.tsx
@@ -0,0 +1,10 @@
+export const Outbox = () => {
+    return (
+        <div className="outbox-container">
+            <form>
+                <input type="text" className="outbox-field" id="" />
+                <button className="outbox-button" > Send </button>
+            </form>
+        </div>
+    )   
+}
